87ed7709a5f33e125cd4f187cfc669268e7fe918
Dale Wright <dale.wright@mbhci.nhs.uk>
2022-10-04 14:39:10 +0100

    2/3 rules now only above/below mean

--- UHMB_SPC_annotations.js
+++ UHMB_SPC_annotations.js
@@ -1142,14 +1142,16 @@ define(["qlik", "jquery", "./d3.min", "css!./UHMB_SPC_annotations.css"],
         }
         function nearUCLCheck(arr,start,num)
         {
-            var output = 0
+            var output = 0;
+            var abovemean = 0;
             if (start + num <= arr.length) {
                 for (var i = 0; i < num; i++) {
-                    output = output + ((arr[start + i].value  >= 2*arr[start + i].currSigma + arr[start + i].currAvg ) ? 1 : 0);
+                    output = output + ((arr[start + i].value  >= 2*arr[start + i].currSigma + arr[start + i].currAvg  && arr[start + i].value  <= 3*arr[start + i].currSigma + arr[start + i].currAvg) ? 1 : 0);
+                    abovemean = abovemean + ((arr[start + i].value  >= arr[start + i].currAvg ) ? 1 : 0);
                 }
-                if(output>=2){
+                if(output>=2 && abovemean ==3){
                     for (var i = 0; i < num; i++) {
-                        if(arr[start + i].value>= (2*arr[start + i].currSigma + arr[start + i].currAvg)){
+                        if(arr[start + i].value>= (2*arr[start + i].currSigma + arr[start + i].currAvg) && arr[start + i].value  <= (3*arr[start + i].currSigma + arr[start + i].currAvg)){
                             arr[start + i].nearUCLCheck = 1;
                         }
                         
@@ -1161,14 +1163,16 @@ define(["qlik", "jquery", "./d3.min", "css!./UHMB_SPC_annotations.css"],
         }
         function nearLCLCheck(arr,start,num)
         {
-            var output = 0
+            var output = 0;
+            var belowmean = 0;
             if (start + num <= arr.length) {
                 for (var i = 0; i < num; i++) {
-                    output = output + ((arr[start + i].value  <= -2*arr[start + i].currSigma + arr[start + i].currAvg ) ? 1 : 0);
+                    output = output + ((arr[start + i].value  <= -2*arr[start + i].currSigma + arr[start + i].currAvg && arr[start + i].value  >= -3*arr[start + i].currSigma + arr[start + i].currAvg ) ? 1 : 0);
+                    belowmean = belowmean + ((arr[start + i].value  <= arr[start + i].currAvg ) ? 1 : 0);
                 }
-                if(output>=2){
+                if(output>=2 && belowmean == 3){
                     for (var i = 0; i < num; i++) {
-                        if(arr[start + i].value<= (-2*arr[start + i].currSigma + arr[start + i].currAvg)){
+                        if(arr[start + i].value<= (-2*arr[start + i].currSigma + arr[start + i].currAvg) && arr[start + i].value  >= -3*arr[start + i].currSigma + arr[start + i].currAvg){
                             arr[start + i].nearLCLCheck = 1;
                         }
                         
